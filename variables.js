035f2deb1d148696d0e7a7382dad0175fe670d9c
omanias <ojmanias1985@gmail.com>
2023-04-20 08:32:48 -0300

    variables added

---
+++ variables.js
@@ -0,0 +1,69 @@
+// VARIABLES
+
+/* let comision = "CoderHouse"; // string => cadena de texto
+
+let edad = 27; // Number => numeros
+
+let disponible = false; // Boolean => true o false
+
+disponible = 89;
+
+const nombre = "Favio";
+
+nombre = "Max"; */
+
+//DIFERENCIAS DE CONST Y LET
+/*
+LET
+1) puedo declarar una variable vacia.
+2) Puedo reasignar valores.
+let nombre;
+nombre = "Harvey";
+nombre = true;
+CONST
+1) no puedo declarar una constante vacia.
+2) no puedo reasignar valores.
+const nombre = "Harvey";
+nombre = true;
+*/
+
+//CONCATENACION => agregar
+/* const nombreAlumno = "Nelson";
+const apellidoAlumno = "Gomez";
+
+const nombreCompleto = nombreAlumno + " " + apellidoAlumno;
+
+console.log(nombreCompleto); */
+
+//console.log(parametros)
+/* prompt("Ingrese su nombre");
+
+alert("Soy una alerta"); */
+
+/* let nombreAlumno = prompt("Ingrese el nombre del alumno");
+
+alert("Nombre del alumno" + " " + nombreAlumno); */
+
+//OPERADORES ARITMETICOS
+
+/*
++ SUMA
+- RESTA
+/ DIVISION
+* MULTIPLICACION
+*/
+
+/* let primerNumero = parseInt(prompt("Ingrese un numero"));
+let segundoNumero = Number(prompt("Ingrese otro numero"));
+
+let suma = primerNumero + segundoNumero;
+let resta = primerNumero - segundoNumero;
+let multiplicacion = primerNumero * segundoNumero;
+let division = primerNumero / segundoNumero;
+
+alert(division); */
+
+/*
+numeros: enteros o flotantes
+intiger o float
+*/
